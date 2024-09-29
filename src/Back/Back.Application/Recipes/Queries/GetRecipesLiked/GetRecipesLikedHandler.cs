28e23057e41438a5e323ce6bf0eb4068da430f36
Nikola Vukasinovic <70863561+vukasinovic21@users.noreply.github.com>
2024-09-29 05:26:50 +0200

    get users liked recipes

---
+++ src/Back/Back.Application/Recipes/Queries/GetRecipesLiked/GetRecipesLikedHandler.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Back.Application.Recipes.Queries.GetRecipesLiked
+{
+
+    public class GetRecipesLikedHandler(IApplicationDbContext dbContext)
+        : IQueryHandler<GetRecipesLikedQuery, GetRecipesLikedResult>
+    {
+        public async Task<GetRecipesLikedResult> Handle(GetRecipesLikedQuery query, CancellationToken cancellationToken)
+        {
+            //get recipes that are liked by UserId using dbContext
+            //return result
+
+            var likes = await dbContext.Likes
+                .Where(l => l.UserId == UserId.Of(query.UserId))
+                .ToListAsync();
+
+            List<Recipe> allRecipes = new List<Recipe>();
+
+            foreach (var l in likes)
+            {
+                var oneRecipe = await dbContext.Recipes
+                    .Include(r => r.RecipeItems)
+                    .AsNoTracking()
+                    .Where(r => r.Id == l.RecipeId)
+                    .FirstAsync();
+                allRecipes.Add(oneRecipe);
+            }
+
+            return new GetRecipesLikedResult(allRecipes.ToRecipeDtotoList());
+        }
+    }
+}
---
+++ src/Back/Back.Application/Recipes/Queries/GetRecipesLiked/GetRecipesLikedQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace Back.Application.Recipes.Queries.GetRecipesLiked
+{
+
+    public record GetRecipesLikedQuery(Guid UserId)
+  : IQuery<GetRecipesLikedResult>;
+
+    public record GetRecipesLikedResult(IEnumerable<RecipeDto> Recipes);
+}
