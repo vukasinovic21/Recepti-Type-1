7203a3e2b056f57f8d42515327ee6a9246e0349d
Nikola Vukasinovic <70863561+vukasinovic21@users.noreply.github.com>
2024-09-29 16:39:50 +0200

    +

--- src/Front/src/app/user-recipe/user-recipe.component.ts
+++ src/Front/src/app/user-recipe/user-recipe.component.ts
@@ -25,6 +25,8 @@ export class UserRecipeComponent
   filteredRecipes: Recipe[] = [];
   likedRecipes: Recipe[] = [];
 
+  showLiked = false;
+
   showAlert = false;
   deleteThis = '';
 
@@ -38,6 +40,12 @@ export class UserRecipeComponent
 
   allTypesOfFood: TypeOfFood[] = [];
   allUsers: UserInfo[] = [];
+
+  like: Like = 
+    {
+      recipeId: '',
+      userId: ''
+    };  
   
   constructor(
     private recipeService: RecipeService,
@@ -76,6 +84,14 @@ export class UserRecipeComponent
       //console.log("LAJKOVANI", recipes)
       this.likedRecipes = recipes;
     })
+  }
+
+  switchRecipes(sortValue: string):void
+  {
+    if(sortValue == 'mine')
+      this.showLiked = false;
+    else
+      this.showLiked = true;
   }
 
   delete(id: string): void
@@ -183,6 +199,11 @@ export class UserRecipeComponent
     this.filteredRecipes = this.recipes.filter(
       recipe => recipe.recipeName.toLowerCase().includes(searchTerm))
 
+    /*let rec = this.likedRecipes.filter(
+      recipe => recipe.recipeName.toLowerCase().includes(searchTerm))
+
+    this.likedRecipes = rec;*/
+
     this.sortRecipes(this.sortOrder);  
   }
 
@@ -194,26 +215,32 @@ export class UserRecipeComponent
     if(this.sortOrder === "timeLowHigh")
     {
       this.filteredRecipes.sort((a,b) => a.timeToPrepare - b.timeToPrepare)
+      this.likedRecipes.sort((a,b) => a.timeToPrepare - b.timeToPrepare)
     }
     else if(this.sortOrder === "timeHighLow")
     {
       this.filteredRecipes.sort((a,b) => b.timeToPrepare - a.timeToPrepare)
+      this.likedRecipes.sort((a,b) => b.timeToPrepare - a.timeToPrepare)
     }
     else if(this.sortOrder === "nameA-Z")
     {
       this.filteredRecipes.sort((a, b) => a.recipeName.localeCompare(b.recipeName));
+      this.likedRecipes.sort((a,b) => b.timeToPrepare - a.timeToPrepare)
     }
     else if(this.sortOrder === "nameZ-A")
     {
       this.filteredRecipes.sort((a, b) => b.recipeName.localeCompare(a.recipeName));
+      this.likedRecipes.sort((a,b) => b.timeToPrepare - a.timeToPrepare)
     }
     else if(this.sortOrder === "newest")
     {
       this.filteredRecipes.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+      this.likedRecipes.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
     }
     else if(this.sortOrder === "oldest")
     {
       this.filteredRecipes.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
+      this.likedRecipes.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
     }
     else if(this.sortOrder === "likeLowHigh")
     {
@@ -253,6 +280,26 @@ export class UserRecipeComponent
     this.recipeService.getAllRecipesPage(this.currentPage-1, this.recipesPerPage).subscribe( recipes => {
       this.recipes = recipes;
       this.filteredRecipes = recipes; 
+    });
+  }
+
+  likeRecipe(recipeId:string): void
+  {
+    this.like.recipeId = recipeId;
+    let userid = localStorage.getItem("userid"); 
+    if(userid)
+    {
+      this.like.userId = userid; 
+    }
+    
+    this.recipeService.likeRecipe(this.like).subscribe({
+      next: (str) => {
+        if(str == '00000000-0000-0000-0000-000000000000')
+        {
+          console.log("Uspesno ste otpratili recept.");
+          this.router.navigate(['recipes/user/'+ this.userId], { queryParams: { refresh: new Date().getTime() }})
+        }
+      }
     });
   }
 }
