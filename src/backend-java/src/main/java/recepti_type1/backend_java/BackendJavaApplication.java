71f41f6177d24aeec0d5e339704225756238a3e8
Nikola Vukasinovic <70863561+vukasinovic21@users.noreply.github.com>
2025-03-04 15:01:01 +0100

    Added java project

---
+++ src/backend-java/src/main/java/recepti_type1/backend_java/BackendJavaApplication.java
@@ -0,0 +1,13 @@
+package recepti_type1.backend_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/backend-java/src/test/java/recepti_type1/backend_java/BackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package recepti_type1.backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
