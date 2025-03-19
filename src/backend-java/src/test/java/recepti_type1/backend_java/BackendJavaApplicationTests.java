78fe7aa034e297c072919bbec855aa4742f66a13
Nikola Vukasinovic <70863561+vukasinovic21@users.noreply.github.com>
2025-03-19 22:06:36 +0100

    Added selenium and junit test in java microservice and added first test

--- src/backend-java/src/test/java/recepti_type1/backend_java/BackendJavaApplicationTests.java
+++ src/backend-java/src/test/java/recepti_type1/backend_java/BackendJavaApplicationTests.java
@@ -1,13 +1,56 @@
 package recepti_type1.backend_java;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BackendJavaApplicationTests {
+import java.time.Duration;
 
-	@Test
-	void contextLoads() {
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class BackendJavaApplicationTests
+{
+
+	WebDriver driver;
+	WebDriverWait wait;
+
+	@BeforeEach // Ovo se izvrsava pre svih testova iz klase
+    public void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\DOWNLOADS\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+
+		driver = new ChromeDriver(); // Pravi novu instancu Chrome driver-a
+
+		driver.get("http://localhost:4200/home"); // Da krene od Home page-a
+		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Ceka 10 sekundi an drajver
 	}
 
+	@Test // Jedan od testova u klasi
+	void loadAllRecipes()
+	{
+		WebElement button = driver.findElement(By.xpath("/html/body/app-root/app-home/div/button"));
+
+		assertTrue(button.isDisplayed());
+		button.click();
+
+		WebElement title = driver.findElement(By.xpath("/html/body/app-root/app-all-recipes/div[1]/div/h1"));
+
+		assertTrue(title.isDisplayed());
+	}
+
+
+	@AfterEach // Ovo se izvrsava posle svih testova iz klase
+	public void CloseConnection()
+	{
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.quit();
+	}
 }
